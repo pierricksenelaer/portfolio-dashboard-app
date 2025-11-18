11c7f89e048c8d3262423367064773f319f2317f
Pierrick Senelaer <pierrick419@gmail.com>
2025-11-18 20:46:04 +0000

    explicitly defining the Turbopack root & move msw from devDependencies to regular dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  // Using 'as any' to bypass the TypeScript strict check on 'experimental'
+  experimental: {
+    // Setting root here
+    turbopack: { 
+      root: './', 
+    },
+  } as any, 
 };
 
 export default nextConfig;
