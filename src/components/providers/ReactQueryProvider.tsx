c12f022e43efba458a6b71e191b8dfeb5344a871
Pierrick Senelaer <pierrick419@gmail.com>
2025-11-19 12:51:32 +0000

    disabling reactQuery retries locally - so no more 404 error console.logs

--- src/components/providers/ReactQueryProvider.tsx
+++ src/components/providers/ReactQueryProvider.tsx
@@ -15,7 +15,9 @@ export default function ReactQueryProvider({ children }: { children: React.React
       queries: {
         // Global default: set staleTime to 5 minutes (5 * 60 * 1000)
         // This prevents immediate refetching if data is considered "fresh"
-        staleTime: 1000 * 60 * 5, 
+        staleTime: 1000 * 60 * 5,
+        // Disable retries in development, since MSW should work immediately. (404 console.logds removal)
+        retry: process.env.NODE_ENV === 'development' ? 0 : 3,
       },
     },
   }));
